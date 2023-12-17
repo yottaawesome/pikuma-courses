c8638b21a1a622a66e23a554bb22b6ea43684623
Vasilios Magriplis <etameson@gmail.com>
2023-12-18 10:14:26 +1100

    More changes

--- src/3d-computer-graphics-programming/renderer/main.cpp
+++ src/3d-computer-graphics-programming/renderer/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+#ifdef _DEBUG // we get undefined symbol linker errors otherwise
+#pragma comment(lib, "SDL2d.lib")
+#pragma comment(lib, "sdl2maind.lib")
+#else
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "sdl2main.lib")
+#endif
 
 int main(int argc, char* argv[])
 {
