202222f6bf1650923629844a75f8c2ddb5bc81cc
Vasilios Magriplis <etameson@gmail.com>
2023-12-18 10:17:56 +1100

    Minor changes

--- src/3d-computer-graphics-programming/renderer/main.cpp
+++ src/3d-computer-graphics-programming/renderer/main.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
+import std;
+// we get undefined symbol linker errors otherwise, requires subsystem:console
+// https://stackoverflow.com/a/50087608/7448661
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
-#ifdef _DEBUG // we get undefined symbol linker errors otherwise
-#pragma comment(lib, "SDL2d.lib")
-#pragma comment(lib, "sdl2maind.lib")
-#else
-#pragma comment(lib, "SDL2.lib")
-#pragma comment(lib, "sdl2main.lib")
-#endif
+// Alternative way of solving the above linker issue with subsystem:windows
+//#ifdef _DEBUG // we get undefined symbol linker errors otherwise
+//#pragma comment(lib, "SDL2d.lib")
+//#pragma comment(lib, "sdl2maind.lib")
+//#else
+//#pragma comment(lib, "SDL2.lib")
+//#pragma comment(lib, "sdl2main.lib")
+//#endif
 
 int main(int argc, char* argv[])
 {
