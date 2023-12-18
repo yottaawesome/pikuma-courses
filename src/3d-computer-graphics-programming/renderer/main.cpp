bdc85a2ffeacd61d2d4cd6a2810aac714d3afb76
Vasilios Magriplis <etameson@gmail.com>
2023-12-18 13:20:25 +1100

    Minor changes

--- src/3d-computer-graphics-programming/renderer/main.cpp
+++ src/3d-computer-graphics-programming/renderer/main.cpp
@@ -1,21 +1,13 @@
 import std;
-// we get undefined symbol linker errors otherwise, requires subsystem:console
-// https://stackoverflow.com/a/50087608/7448661
-#define SDL_MAIN_HANDLED
+// If using subsystem:console, define this to avoid undefined symbol linker errors otherwise.
+// See https://stackoverflow.com/a/50087608/7448661.
+// With subsystem:windows, you need to use WinMain() and not regular main().
+//#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
-// Alternative way of solving the above linker issue with subsystem:windows.
-// Note that it requires setting the library dirs to where vcpkg has built them.
-//#ifdef _DEBUG // we get undefined symbol linker errors otherwise
-//#pragma comment(lib, "SDL2d.lib")
-//#pragma comment(lib, "sdl2maind.lib")
-//#else
-//#pragma comment(lib, "SDL2.lib")
-//#pragma comment(lib, "sdl2main.lib")
-//#endif
-
-int main(int argc, char* argv[])
+int WinMain(int argc, char* argv[])
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello World!\n";
     return 0;
 }
