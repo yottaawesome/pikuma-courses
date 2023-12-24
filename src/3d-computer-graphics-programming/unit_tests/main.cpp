e111f4c381c67a0ae863e7a55c699d5a505407c1
Vasilios Magriplis <etameson@gmail.com>
2023-12-24 20:57:32 +0930

    Minor changes

--- src/3d-computer-graphics-programming/unit_tests/main.cpp
+++ src/3d-computer-graphics-programming/unit_tests/main.cpp
@@ -2,9 +2,6 @@ import std;
 import shared;
 import unit_tests;
 
-template<typename... input_t>
-using tuple_cat_t = decltype(std::tuple_cat(std::declval<input_t>()...));
-
 int main()
 {
     using namespace unit_tests;
