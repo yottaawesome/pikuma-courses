cf85a10a9ab7b5273e45d2fe9b600d459e4a843f
Vasilios Magriplis <etameson@gmail.com>
2026-06-03 22:30:35 +1000

    Add assets and continue 2D engine course.

--- src/2d-game-engine-with-cpp-and-lua/2d-game-engine/main.cpp
+++ src/2d-game-engine-with-cpp-and-lua/2d-game-engine/main.cpp
@@ -1,5 +1,12 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <SDL3/sdl.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <sol/sol.hpp>
 import std;
 
 auto wWinMain(
@@ -8,6 +15,24 @@ auto wWinMain(
     LPWSTR lpCmdLine,
     int nCmdShow
 ) -> int
+try
 {
+    auto lua = sol::state{};
+	lua.open_libraries(sol::lib::base, sol::lib::package);
+
+	auto velocity = glm::vec2{ 5.0f, -2.0f };
+	velocity = glm::normalize(velocity);
+
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS);
     return 0;
+}
+catch (const std::exception& e)
+{
+	MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
+	return -1;
+}
+catch (...)
+{
+	MessageBoxA(nullptr, "An unknown error occurred.", "Error", MB_OK | MB_ICONERROR);
+	return -1;
 }
